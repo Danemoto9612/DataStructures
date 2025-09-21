ac52bc8b2db6b715b6f1a165b1316b03c0869782
Danemoto9612 <danielmortor1202tdea@gmail.com>
2025-09-21 18:17:34 -0500

    UPD package set

--- DataStructures/src/main/java/set/Operations.java
+++ DataStructures/src/main/java/set/Operations.java
@@ -3,4 +3,24 @@ package set;
 
 public class Operations {
     
+    public static void fillSet() {};
+    
+    public static void showSet() {};
+    
+    public static void insertSet() {};
+    
+    public static void deleteSet() {};
+    
+    public static void searchSet() {};
+    
+    public static void modifySet() {};
+    
+    public static void unionSet() {};
+    
+    public static void intersectionSet() {};
+    
+    public static void differenceSet() {};
+    
+    public static void complementSet() {};
+    
 }
