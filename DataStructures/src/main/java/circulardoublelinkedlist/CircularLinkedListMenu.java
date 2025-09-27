47e87aac04a721d575913594c957e4dd40115c8c
Danemoto9612 <danielmortor1202tdea@gmail.com>
2025-09-27 10:26:47 -0500

    ADD new packages circularlinkedlist, circulardoublelinkedlist

---
+++ DataStructures/src/main/java/circulardoublelinkedlist/CircularLinkedListMenu.java
@@ -0,0 +1,5 @@
+package circulardoublelinkedlist;
+
+public class CircularLinkedListMenu {
+    
+}
--- DataStructures/src/main/java/daniel/datastructures/MainMenu.java
+++ DataStructures/src/main/java/daniel/datastructures/MainMenu.java
@@ -16,101 +16,114 @@ public class MainMenu {
     static String option;
 
     public static void structuresMenu() {
-        
+
         // Ciclo para controlar el menú principal de acceso a las demás clases con sus operaciones
         do {
-            
+
             System.out.println("-----------------------------------------------------------------------------");
             System.out.println("                     D A T A   S T R U C T U R E S");
             System.out.println("-----------------------------------------------------------------------------");
-            System.out.println("0. Exit                                                        1. Array");
-            System.out.println("2. Matrix                                                      3. Set  ");
-            System.out.println("4. Arraylist                                                   5. Stack");
-            System.out.println("6. Queue                                                       7. Recursion");
-            System.out.println("8. Tree                                                        9. Graph");
-            System.out.println("10. Record                                                     11. LinkedList");
-            System.out.println("12. Character string                                           13. Easter");
+            System.out.println("0. Exit                                                 1. Array");
+            System.out.println("2. Matrix                                               3. Set  ");
+            System.out.println("4. Arraylist                                            5. Stack");
+            System.out.println("6. Queue                                                7. Recursion");
+            System.out.println("8. Tree                                                 9. Graph");
+            System.out.println("10. Record                                              11. LinkedList");
+            System.out.println("12. Circular linkedlist                                 13. Double linkedList");
+            System.out.println("14. Circular double linkedlist                          15. Character string");
             System.out.println("-----------------------------------------------------------------------------");
             System.out.print("Enter option: ");
             option = sc.nextLine();
-            
+
             switch (option) {
-                
+
                 case "0" -> {
-                    
+
                     System.out.println("-----------------------------------------------------------------------------");
                     System.out.println("                          S E E   Y O U   L A T E R");
                     System.out.println("-----------------------------------------------------------------------------");
                     break;
                 }
                 case "1" -> {
-                    
+
                     ArrayMenu.operationsMenu();
                     break;
                 }
                 case "2" -> {
-                    
+
                     MatrixMenu.operationsMenu();
                     break;
                 }
                 case "3" -> {
-                    
+
                     SetMenu.operationsMenu();
                     break;
                 }
                 case "4" -> {
-                    
+
                     break;
                 }
                 case "5" -> {
-                    
+
                     break;
                 }
                 case "6" -> {
-                    
+
                     break;
                 }
                 case "7" -> {
-                    
+
                     break;
                 }
                 case "8" -> {
-                    
+
                     break;
                 }
                 case "9" -> {
-                    
+
                     break;
                 }
                 case "10" -> {
-                    
+
                     RecordMenu.operationsMenu();
                     break;
                 }
                 case "11" -> {
-                    
+
                     LinkedListMenu.operationsMenu();
                     break;
                 }
                 case "12" -> {
-                    
+
                     StringMenu.operationsMenu();
                     break;
                 }
                 case "13" -> {
                     
+                    break;
+                }
+                case "14" -> {
+                    
+                    break;
+                }
+                case "15" -> {
+                    
+                    break;
+                }
+                case "16" -> {
+
                     System.out.println("-----------------------------------------------------------------------------");
                     System.out.println("                        CREATED BY D4N13L M0R4L35");
                     break;
                 }
                 default -> {
-                    
+
                     System.out.println("-----------------------------------------------------------------------------");
                     System.out.println("                       I N V A L I D   O P T I O N");
                     break;
                 }
             }
-            
+
         } while (!option.equals("0"));
     }
 }
