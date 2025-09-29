fd7558d60cfc78e2c6ad4b41c6fb9822ed8bbe37
Danemoto9612 <danielmortor1202tdea@gmail.com>
2025-09-29 17:49:12 -0500

    ADD new method to tree package

--- DataStructures/src/main/java/tree/TreeMenu.java
+++ DataStructures/src/main/java/tree/TreeMenu.java
@@ -1,6 +1,14 @@
-
 package tree;
 
+import java.util.Scanner;
+
 public class TreeMenu {
+    
+    static Scanner sc = new Scanner(System.in);
+
+    static String option;
+    
+    public static void operationsMenu() {
+    }
     
 }
