8254b9be0c5d2c6ca5e651c08bf28b9c06e79357
Danemoto9612 <danielmortor1202tdea@gmail.com>
2025-10-25 22:21:23 -0500

    UPD package tree

--- DataStructures/src/main/java/tree/Operations.java
+++ DataStructures/src/main/java/tree/Operations.java
@@ -1,6 +1,22 @@
-
 package tree;
 
 public class Operations {
     
+    static NodeTree root = null;
+
+    public static void loadNode() {
+        
+        if (root == null) {
+        } else {
+        }
+    }
+
+    public static void inorder() {
+    }
+
+    public static void preorder() {
+    }
+
+    public static void postorder() {
+    }
 }
--- DataStructures/src/main/java/tree/TreeMenu.java
+++ DataStructures/src/main/java/tree/TreeMenu.java
@@ -3,12 +3,66 @@ package tree;
 import java.util.Scanner;
 
 public class TreeMenu {
-    
+
     static Scanner sc = new Scanner(System.in);
 
     static String option;
-    
+
     public static void operationsMenu() {
+
+        do {
+
+            System.out.println("-----------------------------------------------------------------------------");
+            System.out.println("                                  T R E E ");
+            System.out.println("-----------------------------------------------------------------------------");
+            System.out.println("0. Exit                                                          1. Return");
+            System.out.println("2. Load                                                          3. Inorder");
+            System.out.println("4. Preorder                                                      5. Postorder");
+            System.out.println("-----------------------------------------------------------------------------");
+            System.out.print("Enter option: ");
+            option = sc.nextLine();
+
+            switch (option) {
+
+                case "0" -> {
+
+                    // Permite salir de programa desde este punto
+                    System.out.println("-----------------------------------------------------------------------------");
+                    System.out.println("                          S E E   Y O U   L A T E R");
+                    System.out.println("-----------------------------------------------------------------------------");
+                    System.exit(0);
+                }
+                case "1" -> {
+
+                    // Permite regresar al menú principal
+                    System.out.println("-----------------------------------------------------------------------------");
+                    System.out.println("                <--- <--- <--- R E T U R N <--- <--- <---");
+                    break;
+                }
+                case "2" -> {
+                }
+                case "3" -> {
+                }
+                case "4" -> {
+                }
+                case "5" -> {
+                }
+                case "6" -> {
+
+                    // Permite mostrar un huevo de pascua en el programa
+                    System.out.println("-----------------------------------------------------------------------------");
+                    System.out.println("                        CREATED BY D4N13L M0R4L35");
+                    break;
+                }
+                default -> {
+
+                    // Permite mostrarle al usuario que la opción escogida no es válida
+                    System.out.println("-----------------------------------------------------------------------------");
+                    System.out.println("                       I N V A L I D   O P T I O N");
+                    break;
+                }
+            }
+        } while (!option.equals("1"));
     }
-    
+
 }
